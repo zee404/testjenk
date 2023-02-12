cd6ec513b854947b9c2080fee0ab07e66326dd08
zee <rajazeeshan410@gmail.com>
2023-02-12 17:21:16 +0500

    pushing jenkiswithGit.java

---
+++ jenkiswithGit.java
@@ -0,0 +1,18 @@
+package miscelleneous;
+
+/**
+*
+* @author Zafar Iqbal Khan, zkhan@psu.edu. sa
+*/
+
+public class Jenkinswithgit {
+	public static void main(String[] args) {
+		//code to print Unicode characters
+		int charPerLine=0;
+		for (int 1 = 33; 1 < 256; i++, charPerLine++)
+		{
+		System.out.print((char){+((charPerLine%10--0)?"\n*:"\t"));
+		System. out .print ();
+		}
+	}
+}
